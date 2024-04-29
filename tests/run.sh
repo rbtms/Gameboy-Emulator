f8a65824185057c90b852c214be125d2faeb3d55
Alvaro Fernandez <nishinishi9999@protonmail.com>
2024-04-29 17:17:44 +0200

    Added functionality to run tests containing a command line argument on run.sh.

--- tests/run.sh
+++ tests/run.sh
@@ -3,16 +3,32 @@
 # Helper script to run tests
 #
 
-target_file=$1 # May be test, blargg or mooneye
-target_test=$2
-args=""
+TESTS_TO_RUN=() # Array of tests to run
+target_file=$1  # File containing the tests
+target_test=$2  # Tests containing this word will be executed
+
+
+# Gets a list of all available tests and filters those containing the first argument
+function filter_tests() {
+    filter=$1
+
+    for test in $(cargo test -- --list 2>/dev/null | grep $filter); do
+        if [[ "$test" =~ ^"tests::" ]]; then
+            TESTS_TO_RUN+=(${test::-1})
+        fi
+    done
+}
+
 
 # Run specific test
 if [ -n "$target_test" ]; then
-    args=" tests::$target_test --exact --show-output"
+    filter_tests $target_test
+    
+    cmd="cargo test --package gb --test $target_file -- ${TESTS_TO_RUN[@]} --exact --show-output --test-threads=1"
+# Run all tests of a file
+else
+    cmd="cargo test --package gb --test $target_file -- --show-output --test-threads=1"
 fi
-
-cmd="cargo test --package gb --test $target_file --$args --test-threads=1"
 
 echo cmd: $cmd
 echo
